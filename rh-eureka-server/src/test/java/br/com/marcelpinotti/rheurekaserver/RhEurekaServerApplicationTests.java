0816b818fc0faa9909fc116bef5e6e0bd7cfb8ca
marcelpinotti <marcel.pinotti@gmail.com>
2022-05-11 13:12:08 -0300

    Projeto rh-eureka-server criado

---
+++ rh-eureka-server/src/test/java/br/com/marcelpinotti/rheurekaserver/RhEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.rheurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

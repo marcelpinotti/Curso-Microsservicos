d5a285441eed413faa9d465092b265d6d32e67eb
marcelpinotti <marcel.pinotti@gmail.com>
2022-05-11 10:49:39 -0300

    FeignClient

---
+++ rh-folha-de-pagamento/src/main/java/br/com/marcelpinotti/rhfolhadepagamento/feignclients/TrabalhadorFeignClient.java
@@ -0,0 +1,17 @@
+package br.com.marcelpinotti.rhfolhadepagamento.feignclients;
+
+import br.com.marcelpinotti.rhfolhadepagamento.entities.Trabalhador;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "rh-trabalhador", url = "localhost:8001", path = "/trabalhadores")
+public interface TrabalhadorFeignClient {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Trabalhador> buscarPorId(@PathVariable Long id);
+
+}
--- rh-folha-de-pagamento/src/main/java/br/com/marcelpinotti/rhfolhadepagamento/services/PagamentoService.java
+++ rh-folha-de-pagamento/src/main/java/br/com/marcelpinotti/rhfolhadepagamento/services/PagamentoService.java
@@ -2,6 +2,7 @@ package br.com.marcelpinotti.rhfolhadepagamento.services;
 
 import br.com.marcelpinotti.rhfolhadepagamento.entities.Pagamento;
 import br.com.marcelpinotti.rhfolhadepagamento.entities.Trabalhador;
+import br.com.marcelpinotti.rhfolhadepagamento.feignclients.TrabalhadorFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,21 +14,24 @@ import java.util.Map;
 @Service
 public class PagamentoService {
 
-    @Value("${rh-trabalhador.host}")
-    private String trabalhadorHost;
+   //@Value("${rh-trabalhador.host}")
+    //private String trabalhadorHost;
 
+    //@Autowired
+    //private RestTemplate restTemplate;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private TrabalhadorFeignClient feignClient;
+
 
     public Pagamento getPagamento(long trabalhadorId, int dias) {
 
-        String trabalhadorIdStr = String.valueOf(trabalhadorId);
+        //String trabalhadorIdStr = String.valueOf(trabalhadorId);
+        //Map<String, String> variaveisDaUri = new HashMap<>();
+        //variaveisDaUri.put("id", trabalhadorIdStr);
+        //Trabalhador trabalhador = restTemplate.getForObject(trabalhadorHost + "/trabalhadores/{id}", Trabalhador.class, variaveisDaUri);
 
-        Map<String, String> variaveisDaUri = new HashMap<>();
-        variaveisDaUri.put("id", trabalhadorIdStr);
-
-        Trabalhador trabalhador = restTemplate.getForObject(trabalhadorHost + "/trabalhadores/{id}", Trabalhador.class, variaveisDaUri);
+        Trabalhador trabalhador = feignClient.buscarPorId(trabalhadorId).getBody();
         return new Pagamento(trabalhador.getNome(), trabalhador.getValorPorDia(), dias);
     }
 }

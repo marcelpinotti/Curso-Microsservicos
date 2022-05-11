11432c2cf06c7231b5a1d46e86444798fef3e223
marcelpinotti <marcel.pinotti@gmail.com>
2022-05-10 22:29:24 -0300

    Projeto rh-trabalhadores criado

---
+++ rh-trabalhadores/src/main/java/br/com/marcelpinotti/rhtrabalhadores/RhTrabalhadoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.rhtrabalhadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhTrabalhadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhTrabalhadoresApplication.class, args);
+	}
+
+}

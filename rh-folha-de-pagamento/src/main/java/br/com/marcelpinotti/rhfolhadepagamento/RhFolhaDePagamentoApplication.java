ca426c2ecea0d5913964ecf7add338218bd22041
marcelpinotti <marcel.pinotti@gmail.com>
2022-05-10 23:49:14 -0300

    Projeto rh-folha-de-pagamento criado

---
+++ rh-folha-de-pagamento/src/main/java/br/com/marcelpinotti/rhfolhadepagamento/RhFolhaDePagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.rhfolhadepagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhFolhaDePagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhFolhaDePagamentoApplication.class, args);
+	}
+
+}
---
+++ rh-folha-de-pagamento/src/test/java/br/com/marcelpinotti/rhfolhadepagamento/RhFolhaDePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.rhfolhadepagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhFolhaDePagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a3f57cef4be1817b684ee34552c4d53bad1a42f1
marcelpinotti <marcel.pinotti@gmail.com>
2022-05-12 09:42:49 -0300

    Configurando Clientes Eureka Folha de Pagamento e Trabalhadores

--- rh-folha-de-pagamento/src/main/java/br/com/marcelpinotti/rhfolhadepagamento/RhFolhaDePagamentoApplication.java
+++ rh-folha-de-pagamento/src/main/java/br/com/marcelpinotti/rhfolhadepagamento/RhFolhaDePagamentoApplication.java
@@ -2,11 +2,13 @@ package br.com.marcelpinotti.rhfolhadepagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients
-@RibbonClient(name="rh-trabalhador")
+//@RibbonClient(name="rh-trabalhador")
 @SpringBootApplication
 public class RhFolhaDePagamentoApplication {
 

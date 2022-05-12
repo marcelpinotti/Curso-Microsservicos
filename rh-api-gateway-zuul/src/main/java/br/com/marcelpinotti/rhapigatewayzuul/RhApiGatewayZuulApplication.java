b625c164386851a4322772527023dd6e7511e9fe
marcelpinotti <marcel.pinotti@gmail.com>
2022-05-12 11:31:42 -0300

    Projeto rh-api-gateway-zuul criado

---
+++ rh-api-gateway-zuul/src/main/java/br/com/marcelpinotti/rhapigatewayzuul/RhApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.rhapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ rh-api-gateway-zuul/src/test/java/br/com/marcelpinotti/rhapigatewayzuul/RhApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.rhapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

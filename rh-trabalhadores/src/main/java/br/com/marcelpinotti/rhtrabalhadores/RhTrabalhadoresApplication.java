95c86870ea2ff4dcf955479808bf1c69f28b7bad
marcelpinotti <marcel.pinotti@gmail.com>
2022-05-12 13:42:08 -0300

    Configurando Timeout do Zull utilizando as configurações do Hystrix e Ribbon no application.properties

--- rh-trabalhadores/src/main/java/br/com/marcelpinotti/rhtrabalhadores/RhTrabalhadoresApplication.java
+++ rh-trabalhadores/src/main/java/br/com/marcelpinotti/rhtrabalhadores/RhTrabalhadoresApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class RhTrabalhadoresApplication {
+public class RhTrabalhadoresApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RhTrabalhadoresApplication.class, args);

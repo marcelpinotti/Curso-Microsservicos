5b9eb416540792465fa023e36d036ce0c9742129
marcelpinotti <marcel.pinotti@gmail.com>
2022-05-12 11:04:33 -0300

    Hystrix tolerancia a falhas

--- rh-folha-de-pagamento/src/main/java/br/com/marcelpinotti/rhfolhadepagamento/resources/PagamentoResource.java
+++ rh-folha-de-pagamento/src/main/java/br/com/marcelpinotti/rhfolhadepagamento/resources/PagamentoResource.java
@@ -2,6 +2,7 @@ package br.com.marcelpinotti.rhfolhadepagamento.resources;
 
 import br.com.marcelpinotti.rhfolhadepagamento.entities.Pagamento;
 import br.com.marcelpinotti.rhfolhadepagamento.services.PagamentoService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +20,15 @@ public class PagamentoResource {
     @Autowired
     private PagamentoService service;
 
+    @HystrixCommand(fallbackMethod = "efetuarPagamentoAlternativo")
     @GetMapping(value = "/{trabalhadorId}/dias/{dias}")
     public ResponseEntity<Pagamento> efetuarPagamento(@PathVariable Long trabalhadorId , @PathVariable int dias){
         return ResponseEntity.ok(service.getPagamento(trabalhadorId,dias));
+    }
+
+    public ResponseEntity<Pagamento> efetuarPagamentoAlternativo (Long trabalhadorId , int dias) {
+        Pagamento pagamento = new Pagamento("Aguarde alguns segundos", 0.00, dias = 0);
+        return ResponseEntity.ok(pagamento);
+
     }
 }

a2d96dcb0bd0a988756ef86cc8bf0a19781cdeb0
marcelpinotti <marcel.pinotti@gmail.com>
2022-05-14 23:22:03 -0300

    Projeto rh-usuario criado

---
+++ rh-usuario/src/main/java/br/com/marcelpinotti/rhusuario/RhUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.rhusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhUsuarioApplication.class, args);
+	}
+
+}
---
+++ rh-usuario/src/test/java/br/com/marcelpinotti/rhusuario/RhUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.rhusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
